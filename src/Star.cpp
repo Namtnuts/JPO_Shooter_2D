40326b29b8a7af65de17052ff2b1959708b18fb3
Namtnuts <glosu218@gmail.com>
2024-01-21 23:34:26 +0100

    Add all comments

--- src/Star.cpp
+++ src/Star.cpp
@@ -1,6 +1,17 @@
 #include "Classes/Star.h"
 
+// Private functions
 void Star::initShape(sf::Vector2f startPosition){
+    ///////////////////////////////////////////////////////////
+    // This function initialize shape
+    ///////////////////////////////////////////////////////////
+    //		
+    // INPUT:
+    //      startPosition - starting position of shape (x,y)
+    // OUTPUT:
+    //	    shape is initialized
+    // REMARKS:
+
     m_shape.setPointCount(8);
     m_shape.setPoint(0, sf::Vector2f(20.f, 0.f) + startPosition);
     m_shape.setPoint(1, sf::Vector2f(25.f, 15.f) + startPosition);
@@ -13,6 +24,7 @@ void Star::initShape(sf::Vector2f startPosition){
     m_shape.setFillColor(sf::Color::Yellow);
 }
 
+// Constructor and Destructor
 Star::Star(sf::Vector2f startPosition){
     this->initShape(startPosition);
 }
@@ -21,14 +33,35 @@ Star::~Star(){
 
 }
 
+// Accessors
+///////////////////////////////////////////////////////////
+// These functions access private variebles of class
+///////////////////////////////////////////////////////////
 sf::FloatRect Star::getBounds(){
     return m_shape.getGlobalBounds();
 }
 
 void Star::update(){
-
+    ///////////////////////////////////////////////////////////
+    // This function currently does nothing (this function is for future)
+    ///////////////////////////////////////////////////////////
+    //		
+    // INPUT:
+    // OUTPUT:
+    // REMARKS:
+    //      maybe add more features to game
 }
 
 void Star::render(sf::RenderTarget& target){
+    ///////////////////////////////////////////////////////////
+    // This function renders shape
+    ///////////////////////////////////////////////////////////
+    //		
+    // INPUT:
+    //      target - object to render to
+    // OUTPUT:
+    //		shape is rendered
+    // REMARKS:
+
     target.draw(m_shape);
 }
